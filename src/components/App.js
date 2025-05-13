489c5a5832e4b992d5bc75106cd77fd039fe1056
DevTruce <DevTruce@gmail.com>
2025-05-13 15:36:20 -0400

    complete header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import Background from "./Background";
 import Header from "./Header";
+
 export default function App() {
   return (
     <>
       <Background />
-      <main className="container flex flex-col mx-auto">
+      <main className="container flex flex-col mx-auto justify-center items-center">
         <Header />
       </main>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [annualBilling, setAnnualBilling] = useState(false);
+
+  function handleBillingToggle() {
+    setAnnualBilling(cur => !cur);
+  }
+
   return (
-    <header>
-      <h1 className="text-[2rem] font-bold text-lightGrey text-center mt-10">
+    <header className=" container flex flex-col gap-10">
+      <h1 className="text-[2rem] font-bold text-lightGray text-center mt-16">
         Our Pricing
       </h1>
+
+      <div className=" flex flex-row items-center mx-auto gap-6">
+        <span className="text-lightGray font-bold opacity-50">Annually</span>
+
+        <div
+          onClick={handleBillingToggle}
+          data-annual={annualBilling}
+          className={`toggle`}
+        ></div>
+
+        <span className="text-lightGray font-bold opacity-50">Monthly</span>
+      </div>
     </header>
   );
 }
