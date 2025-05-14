a750949760d2182fe8818877217dc2f77773a608
DevTruce <DevTruce@gmail.com>
2025-05-13 22:53:06 -0400

    bg images for desktop/mobile

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,15 @@ export default function App() {
   const [annualBilling, setAnnualBilling] = useState(false);
 
   return (
-    <>
+    <div
+      className="
+    bg-no-repeat 
+    bg-[url('./images/bg-top.svg')] 
+    bg-[position:right_-12rem_top_-2rem]
+    lg:bg-[url('./images/bg-top.svg'),url('./images/bg-bottom.svg')] 
+    lg:bg-[position:right_top,left_bottom]
+"
+    >
       <main className="container flex flex-col mx-auto justify-center items-center pt-16 pb-16">
         <Header
           annualBilling={annualBilling}
@@ -15,6 +23,6 @@ export default function App() {
         />
         <CardList annualBilling={annualBilling} />
       </main>
-    </>
+    </div>
   );
 }
